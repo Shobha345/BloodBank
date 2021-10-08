e62961af719f83b68327801a6d1528d9b48820a4
Cyb10101 <cyb10101@gmail.com>
2021-10-09 00:17:46 +0200

    Fix html header includes

--- head.php
+++ head.php
@@ -1,16 +1,21 @@
 <head>
-	<title><?php echo $title ?></title>
-	<meta charset="UTF-8" />
+    <title><?php echo $title ?></title>
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../library/css/bootstrap.min.css"/>
-    <script type="text/javascript" src="../library/js/jquery-3.2.1.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="/css/style.css" rel="stylesheet"/>
+
+    <!-- Bootstrap JavaScript Bundle (jQuery upgraded) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link href="css/style.css" rel="stylesheet"/>
-    <link rel="shortcut icon" type="image/jpeg" href="image/favicon.jpg">
+
+    <!-- TODO default icon support, must be ico, should be a square -->
+    <!-- <link rel="shortcut icon" href="/images/favicon.ico" /> -->
+    <!-- TODO icon not exists, should be a square -->
+    <link rel="icon" type="image/jpeg" href="/image/favicon.jpg">
 </head>
